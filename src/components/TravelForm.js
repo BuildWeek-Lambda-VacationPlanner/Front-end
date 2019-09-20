a8313a79ee007918e95bd1d6d73442274517fb4b
jacobcalv <jacobcalvino@outlook.com>
2019-09-19 20:06:46 -0500

    got data displaying from form just not in right component quite yet

--- src/components/TravelForm.js
+++ src/components/TravelForm.js
@@ -1,35 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {styled} from 'styled-components'
 import {withFormik, Form, Field} from 'formik'
+import VacationCard from './vacationCard.js'
+import axios from 'axios'
 
-const TravelForm = () => {
+const TravelForm = ({status}) => {
+    const [vacations, setVacations] = useState([])
     const addDestination = () =>{
 
     }
     const addActivity = () => {
 
     }
-    
+    useEffect(()=>{
+        if(status) {
+            setVacations([...vacations, status])
+        }
+    }, [status])
     return (
+        <>
         <Form>
             <h1>Add Your Vacation Details</h1>
             <div className='forms'>
                 <label>
                     <span>Destination/s  </span>
-                    <Field type='text' name='vacation'/>
+                    <Field type='text' name='destination'/>
                     <button onClick={addDestination}>+</button> 
                 </label><br/>
                 <label>
-                    <span>Date Range  </span>
+                    <span>Date Range:  </span>
                     <Field type='date' name='startRange' /> to <Field type='date' name='endRange'/>    
+                </label><br/>
+                <label>
+                    <span>Anticipated Cost: </span>
+                    <Field type='number' name='cost'/>
                 </label>
                 <h3>Activities</h3>
-                <Field type='text' name='activity'/>
+                <Field type='text' name='activity'/><button>+</button>
                 <h3>Notes</h3>
-                <Field type='text' name='notes'/>
+                <Field type='text' name='description'/>
             </div>
             <button type='submit'>Add Vacation</button>
         </Form>
+        <div>
+            {vacations.map(vacation=>(
+                <VacationCard 
+                destination={vacation.destination} 
+                startRange={vacation.startRange}
+                endRange={vacation.endRange}
+                description={vacation.description}
+                cost={vacation.cost}
+                />
+            ))}
+        </div>
+        </>
     )
 }
 
@@ -38,14 +62,23 @@ const TravelForm = () => {
 export default withFormik({
     mapPropsToValues: (values) => {
         return{
-            vacation: values.vacation || '',
+            destination: values.destination || '',
             startRange: values.startRange || '',
             endRange: values.endRange || '',
+            cost: values.cost || '',
             activity: values.activity || '',
-            notes: values.notes || '',
+            description: values.description || '',
         }
     }, 
-    handleSubmit: (values)=>{
+    handleSubmit: (values, {setStatus})=>{
         console.log(values)
+        axios.post('https://reqres.in/api/vacations', values)
+            .then((res)=>{
+                setStatus(res.data)
+                
+            })
+            .catch((err)=> {
+                console.log(err)
+            }) 
     }
 })(TravelForm)

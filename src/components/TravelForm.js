6f036a4f00cb153d2d1c5b1ed2d81ad5dbd521ae
Danielle B <danibaxx@hotmail.com>
2019-09-23 17:39:24 -0600

    updated TravelForm.js with syntax errors for netlify

--- src/components/TravelForm.js
+++ src/components/TravelForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {  useState, useEffect } from 'react'
 import styled from 'styled-components'
 import {withFormik, Form, Field} from 'formik'
 import VacationCard from './vacationCard.js'

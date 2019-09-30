12855e0d28cbfb6d5f09dd4a97ac13d41f84b313
jacobcalv <jacobcalvino@outlook.com>
2019-09-30 18:06:41 -0500

    added more styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -13,13 +13,24 @@ const Dashboard = (props) => {
         color: white;
         border: 3px solid orange;
     `
+    const Header = styled.h1`
+        background-color: #e77727;
+        color: white;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        border: 1px solid black;
+    `
+    const Line = styled.h1`
+        border-bottom: 2px solid black;
+    `
 
     return (
         <div>
             <div>
                 <NavBar />
             </div>
-            <h1>{props.userName}'s Current Vacations <span role='img' aria-label='beach'>🏖️</span></h1>
+            <Header>{props.userName}'s Current Vacations <span role='img' aria-label='beach'>🏖️</span></Header>
+            <Line></Line>
             {props.vacations.map(vacation => <DashboardItem key={vacation.id} destination={vacation.destination} id={vacation.id} start_date={vacation.start_date} end_date={vacation.end_date} /> )}
             <Link to='/newtrip'><Button>Add Trip</Button></Link>
         </div>

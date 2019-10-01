f8d52206fd3a58e00f7c768f5ed9f4c994135e73
jacobcalv <jacobcalvino@outlook.com>
2019-10-01 17:43:59 -0500

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TravelForm from './components/TravelForm'
 import VacationCard from './components/VacationCard'
 import { Route } from 'react-router-dom';
 import axios from 'axios'
-
+import styled from 'styled-components'
 import Header from './components/Header';
 import Login from './components/Login/Login';
 import Home from './components/Home';
@@ -14,7 +14,18 @@ import Register from './components/Login/Register';
 import './App.css';
 
 function App() {
-
+  const App = styled.div`
+    text-align: center;
+    background: transparent;
+    width: 850px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    border-radius: 2px;
+    padding: 18px;
+    margin: 0 auto;
+    box-shadow: 0 3px 6px #ececff, 0 3px 6px #944875;
+  `
   const [vacations, setVacations] = useState([])
   useEffect((token)=>{
     axios.get('https://vacation-planner-bw.herokuapp.com/api/vacations', {headers: {'Authorization' : token }})
@@ -39,7 +50,7 @@ function App() {
   console.log(userVacations)
  
   return (
-    <div className="app">
+    <App>
       <div>
         <Header />
       </div>
@@ -49,7 +60,7 @@ function App() {
       <Route path='/dashboard' render={props => <Dashboard {...props}  userName={userName} vacations={userVacations} />}/>
       <Route path='/newtrip' render={props => <TravelForm {...props} setVacations={setVacations} vacations={vacations} />}/>
       <Route path='/card/:id' render={props => <VacationCard {...props}  vacations={vacations} />}/>
-    </div>
+    </App>
   );
 }
 
